933e4242eaf883a086e359882a1d7d7514d80bb3
henrybi <henrybi@u.washington.edu>
2016-02-22 11:30:53 -0800

    finished stopLight Assignment

--- stoplight-event-exercise/script.js
+++ stoplight-event-exercise/script.js
@@ -8,3 +8,34 @@
   When I click on the 'go' button
     the bottom light should turn green.
 */
+
+
+var stopButton = document.querySelector("#stopButton");
+var slowButton = document.querySelector("#slowButton");
+var goButton = document.querySelector("#goButton");
+
+var stopLight = document.querySelector("#stopLight");
+var slowLight = document.querySelector("#slowLight");
+var goLight = document.querySelector("#goLight");
+
+stopButton.addEventListener("click", stoplightOn);
+slowButton.addEventListener("click", slowlightOn);
+goButton.addEventListener("click", golightOn);
+
+function stoplightOn(){
+  stopLight.style.backgroundColor = "red";
+  slowLight.style.backgroundColor = "black";
+  goLight.style.backgroundColor = "black";
+}
+
+function slowlightOn(){
+  stopLight.style.backgroundColor = "black";
+  slowLight.style.backgroundColor = "yellow";
+  goLight.style.backgroundColor = "black";
+}
+
+function golightOn(){
+  stopLight.style.backgroundColor = "black";
+  slowLight.style.backgroundColor = "black";
+  goLight.style.backgroundColor = "green";
+}
