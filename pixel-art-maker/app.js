fab0ae2177a739f1c28802cd0f8c5501b910b38e
henrybi <henrybi@u.washington.edu>
2016-02-24 12:07:49 -0800

    implemented esLint and passed esLint

--- pixel-art-maker/app.js
+++ pixel-art-maker/app.js
@@ -1,67 +1,72 @@
-var body = document.getElementsByTagName('body')[0];
-var box;
-function createCanvas(){
-  for (var i = 0; i < 2046; i++) {
-    box = document.createElement('div');
-    box.id = i;
-    box.className = "empty";
-    box.style.width = "20px";
-    box.style.height = "20px";
-    box.style.border = "solid 1px";
-    box.style.float = "left";
-    box.style.backgroundColor = "white";
-    body.appendChild(box);
-  }
-}
+(function() {
+  'use strict';
+  var body = document.getElementsByTagName('body')[0];
+  var box;
 
-createCanvas();
+  var createCanvas = function() {
+    for (var i = 0; i < 2046; i++) {
+      box = document.createElement('div');
+      box.id = i;
+      box.className = 'empty';
+      box.style.width = '20px';
+      box.style.height = '20px';
+      box.style.border = 'solid 1px';
+      box.style.float = 'left';
+      box.style.backgroundColor = 'white';
+      body.appendChild(box);
+    }
+  };
 
-function colorPallet(){
-  document.getElementById('0').style.backgroundColor = "red";
-  document.getElementById('1').style.backgroundColor = "orange";
-  document.getElementById('2').style.backgroundColor = "yellow";
-  document.getElementById('3').style.backgroundColor = "green";
-  document.getElementById('4').style.backgroundColor = "blue";
-  document.getElementById('5').style.backgroundColor = "purple";
-  document.getElementById('6').style.backgroundColor = "pink";
-  document.getElementById('7').style.backgroundColor = "brown";
-  document.getElementById('8').style.backgroundColor = "gray";
-  document.getElementById('9').style.backgroundColor = "white";
-  document.getElementById('10').style.backgroundColor = "black";
-  for (var i = 0; i < 11; i++) {
-    document.getElementById(i).className = "colorSelect";
-  }
-}
+  createCanvas();
 
-colorPallet();
+  var colorPallet = function() {
+    document.getElementById('0').style.backgroundColor = 'red';
+    document.getElementById('1').style.backgroundColor = 'orange';
+    document.getElementById('2').style.backgroundColor = 'yellow';
+    document.getElementById('3').style.backgroundColor = 'green';
+    document.getElementById('4').style.backgroundColor = 'blue';
+    document.getElementById('5').style.backgroundColor = 'purple';
+    document.getElementById('6').style.backgroundColor = 'pink';
+    document.getElementById('7').style.backgroundColor = 'brown';
+    document.getElementById('8').style.backgroundColor = 'gray';
+    document.getElementById('9').style.backgroundColor = 'white';
+    document.getElementById('10').style.backgroundColor = 'black';
+    for (var i = 0; i < 11; i++) {
+      document.getElementById(i).className = 'colorSelect';
+    }
+  };
 
-var selectColor;
-var mouseID = false;
+  colorPallet();
 
-body.addEventListener("click", function(event){
-  if (event.target.className == "colorSelect") {
-    selectColor = event.target.style.backgroundColor;
-  } else {
-    event.target.style.backgroundColor = selectColor;
-  }
-});
+  var selectColor;
+  var mouseID = false;
 
-body.addEventListener("mouseover", function(){
-  if (mouseID === true) {
-    event.target.style.backgroundColor = selectColor;
-  }
-});
+  body.addEventListener('click', function(event) {
+    if (event.target.className === 'colorSelect') {
+      selectColor = event.target.style.backgroundColor;
+    }
+    else {
+      event.target.style.backgroundColor = selectColor;
+    }
+  });
 
-body.addEventListener("mousedown", function(){
-  mouseID = true;
-});
+  body.addEventListener('mouseover', function() {
+    if (mouseID === true) {
+      event.target.style.backgroundColor = selectColor;
+    }
+  });
 
-body.addEventListener("mouseup", function(){
-  mouseID = false;
-});
+  body.addEventListener('mousedown', function() {
+    mouseID = true;
+  });
 
-body.addEventListener("change", function(event){
-  var pickColor = document.getElementById("picker").value;
-  selectColor = pickColor;
+  body.addEventListener('mouseup', function() {
+    mouseID = false;
+  });
 
-});
+  body.addEventListener('change', function() {
+    var pickColor = document.getElementById('picker').value;
+
+    selectColor = pickColor;
+  });
+})();
