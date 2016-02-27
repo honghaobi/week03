66ec381c333c1822991f15c750e2bead11606afe
henrybi <henrybi@u.washington.edu>
2016-02-26 16:06:08 -0800

    created initail function. works!

--- memory-game-lab/app.js
+++ memory-game-lab/app.js
@@ -1,7 +1,5 @@
-(function(){
-  "use strict";
-
   var body = document.getElementsByTagName('body')[0];
+  var imgArray = ["red","red","orange","orange","yellow", "yellow", "green", "green", "blue", "blue", "purple", "purple", "brown", "brown", "pink", "pink"];
 
   var createBoxes = function() {
     var boxContainer = document.createElement('section');
@@ -10,10 +8,12 @@
 
     var box;
 
-    for (var i = 1; i <= 16; i++) {
+    for (var i = 0; i < 16; i++) {
       box = document.createElement('div');
       box.id = "box" + i;
       box.className = 'box';
+      box.style.backgroundColor = imgArray[i];
+      box.style.opacity = 0;
       boxContainer.appendChild(box);
     }
   };
@@ -25,16 +25,42 @@
 
     var overLay;
 
-    for (var i = 1; i <= 16; i++) {
+    for (var i = 0; i < 16; i++) {
       overLay = document.createElement('div');
       overLay.className = 'overLay';
       overLayContainer.appendChild(overLay);
     }
   };
 
-  // var showBox = function{
-  //
-  // }
+  var matchColor;
+  var matchBox;
+  var clickCount = 0;
+
+  var showBox = function() {
+    body.addEventListener('click', function(event) {
+      if (!matchColor) {
+        matchBox = event.target.id;
+        matchColor = event.target.style.backgroundColor;
+        clickCount += 1;
+        event.target.style.opacity = 1;
+      } else if (matchColor === event.target.style.backgroundColor) {
+        event.target.style.opacity = 1;
+        matchColor = undefined;
+        clickCount += 1;
+      } else if (matchColor !== event.target.style.backgroundColor){
+        event.target.style.opacity = 1;
+        setTimeout(function(){
+          document.getElementById(matchBox).style.opacity = 0;
+          event.target.style.opacity = 0;
+        },500);
+        setTimeout(function(){
+          matchBox = undefined;
+          matchColor = undefined;
+          clickCount += 1;
+        },501);
+      }
+    });
+  };
 
 
 
@@ -43,5 +69,4 @@
 
   createBoxes();
   createOverlay();
-
-})();
+  showBox();
